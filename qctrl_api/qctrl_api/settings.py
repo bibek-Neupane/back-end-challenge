2467e1b16061a7683ae161830421cb7909305c01
Bibek Neupane <porcupinedev@gmail.com>
2019-12-14 11:20:25 +1100

    postgres database added

--- qctrl_api/qctrl_api/settings.py
+++ qctrl_api/qctrl_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'qctrl_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'qctrl',
+        'USER': 'postgres',
+        'PASSWORD': 's180396#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
